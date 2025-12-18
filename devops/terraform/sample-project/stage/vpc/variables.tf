1cab995fcec59c47d3befaf70b35d2324ab20073
andredsferreira <andredsferreira2@gmail.com>
2025-12-18 17:34:50 +0000

    terraform: sample project starting layout

---
+++ devops/terraform/sample-project/stage/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "The name for the VPC and all other resources"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "The IP address range, in CIDR notation, to use for this VPC."
+  type        = string
+}
