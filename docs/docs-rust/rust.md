3fcd5b3878407f3abdb04d7c701e21e5cf3f8e38
andredsferreira <andredsferreira1@gmail.com>
2026-07-20 23:22:15 +0100

    docker: reviewing some docker concepts and readings on open telemetry

--- docs/docs-rust/rust.md
+++
@@ -1,5 +0,0 @@
-## Rust
-
-**Trait** a collection of methods that a type can implement. Example: FromStr
-trait from std::str::FromStr includes the from_str method. The u64 type for
-example implements that method.
--- labs/lab02/main.go
+++ labs/lab02/main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 
 	"andrekb/lab02/db"
@@ -19,6 +26,25 @@ func main() {
 
 	app.Get("/users", handler.GetUsers)
 
-	app.Listen(":8080")
+	go func() {
+		log.Println("listening on :8080...")
+		if err := app.Listen(":8080"); err != nil {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
 
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Fatalf("forced shutdown: %v", err)
+	}
+
+	log.Println("server stopped")
 }
