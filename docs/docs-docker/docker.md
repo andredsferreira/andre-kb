0d6a03db2eff9d47518f0d003f9cf93c861ed23f
andredsferreira <andredsferreira1@gmail.com>
2026-07-22 23:27:48 +0100

    docker: reviewing some concepts

--- docs/docs-docker/docker.md
+++ docs/docs-docker/docker.md
@@ -1,7 +1,14 @@
-## Docker
+## Building & Dockerfiles
+
+Buildx (client) ----> BuildKit (server)
 
 When writing Dockerfiles place instructions that are not changed frequently
 first. Docker uses a cache and reuses layers that did not change. If it hits an
 instruction that represents a layer that was changed it invalidates the cache on
 **subsequent** instructions, thus we place this instructions more at the bottom.
 
+**Build context** The set of files that Buildx may access in the building stage. It
+can be a local directory, a remote repository, or a tarball. When using "docker
+build .", the dot at the end indicates that the build context is the current
+local working directory. Thus, Dockerfile instructions, such as COPY, may
+reference files in this directory.
--- labs/lab03/main.go
+++ labs/lab03/main.go
@@ -1,1 +1,44 @@
-package lab03
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("LAB03: Fiber Service")
+	})
+
+	go func() {
+		slog.Info("listening on port 8080")
+		if err := app.Listen(":8080"); err != nil {
+			slog.Error("server error", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+	slog.Info("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		slog.Error("forced shutdown", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("server stopped")
+}
